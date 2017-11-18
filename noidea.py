d3c6be74d805cb74688877485168db3030157012
Noble <sanjeevboo026@gmail.com>
2017-11-18 23:51:13 +0530

    SKID

---
+++ noidea.py
@@ -0,0 +1,28 @@
+import discord
+import asyncio
+import json
+bot = discord.Client()
+
+try:
+    file = open("settings/credentials.json",'r+')
+    file = file.read()
+    settings = json.loads(file)
+except Exception as e:
+    print(e)
+
+@bot.event
+async def on_ready():
+    print(bot.user.id)
+    print(bot.user.name)
+
+@bot.event
+async def on_message(message):
+    channel = message.channel
+    prefix = settings["prefix"]
+
+    if message.content.startswith(prefix+'skid'):
+        await bot.send_message(channel, "**OP**")
+
+print("Starting......")
+bot.run(settings["token"])
+#bot.run("")
