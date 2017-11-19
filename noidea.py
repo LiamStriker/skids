6e28dcf6d200edbfe2368ca9ea5b36dd471e657c
LiamStriker <33391338+LiamStriker@users.noreply.github.com>
2017-11-19 08:42:12 +0530

    Hmmskidhmm

--- noidea.py
+++ noidea.py
@@ -14,7 +14,7 @@ except Exception as e:
 async def on_ready():
     print(bot.user.id)
     print(bot.user.name)
-    await bot.change_presence( game=discord.Game( name=settings["prefix"]+"=prefix ~LIAM",type = 0))
+    await bot.change_presence( game=discord.Game( name=settings["prefix"]+"=prefix ~LIAM",type =0))
 
 @bot.event
 async def on_message(message):
@@ -25,10 +25,10 @@ async def on_message(message):
         await bot.send_message(channel, "**OP**")
         
         elif message.content.startswith(prefix+'mika'):
-        await bot.send_message(channel, "Liams OP Bunny Wifey")
+            await bot.send_message(channel, "Liams OP Bunny Wifey")
         
              elif message.content.startswith(prefix+'noble'):
-             await bot.send_message(channel, "**THE OP PYTHON PRO NOOB**")
+                 await bot.send_message(channel, "**THE OP PYTHON PRO NOOB**")
 
         
       print("Starting......")
